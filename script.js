4a1ad7df4424aa172a25587e7e62c0ee4b45f13f
SiamakSm <siamak036@gmail.com>
2025-07-24 18:45:56 +0200

    Completed

--- script.js
+++ script.js
@@ -2,21 +2,48 @@ let startBtn = document.getElementById("startBtn");
 let scoreSpan = document.getElementById("score");
 let time = document.getElementById("inputTime");
 let box = document.getElementById("box");
-
-
+let countdown = document.getElementById("countdown");
 let interval;
 let score;
 
 
 startBtn.addEventListener("click", function () {
-    score = 0;
-    box.style.display = "block";
-    startBtn.style.display = "none";
-    clearInterval(interval);
-    interval = setInterval(() => {
-        moveBox();
-    }, 1000);
-    moveBox();
+    let duration = parseInt(time.value);
+    let remainingTime = duration;
+
+    if (duration > 5) {
+        countdown.style.display = "block";
+        countdown.textContent = `Remaining Time : ${remainingTime} Seconds`;
+        score = 0;
+        startBtn.style.display = "none";
+        box.style.display = "block";
+        time.disabled = true;
+        time.style.display = "none";
+        clearInterval(interval);
+
+        interval = setInterval(() => {
+            moveBox();
+            remainingTime--;
+            if (remainingTime <= 3) {
+                countdown.style.color = "red";
+            }
+            countdown.textContent = `Remaining Time : ${remainingTime} Seconds`;
+
+        }, 1000);
+
+        setTimeout(() => {
+            clearInterval(interval);
+            startBtn.style.display = "block";
+            box.style.display = "none";
+            time.value = 0;
+            time.disabled = false;
+            time.style.display = "block";
+            countdown.style.display = "none";
+            scoreSpan.textContent = `Your score is ${score}`;
+        }, (duration * 1000));
+
+    };
+
 });
 
 
@@ -28,5 +55,4 @@ function moveBox() {
 box.addEventListener("click", function () {
     score++;
     scoreSpan.textContent = score;
-    moveBox();
 });
